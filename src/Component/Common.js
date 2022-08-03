93c5735308477f2c6fc29d0dd22d4bfda9b344ae
Pitambarbittu <99995315+Pitambarbittu@users.noreply.github.com>
2022-08-04 01:36:54 +0530

    react-assessment-1

---
+++ src/Component/Common.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Common = () => {
+  return (
+    <div>
+          <h1>Styling Using Functional and Class Component</h1>
+     
+     <button className="btn1">Div by using Functional Component</button>
+     <button className="btn2">Div by using Class Component</button>
+    </div>
+  )
+}
+
+export default Common;
